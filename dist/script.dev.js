58cd6f91179ea52567933d491bf4229e9d61dddc
isonguyom <isonguyom@gmail.com>
2022-08-18 19:44:08 +0100

    generate number password and create copy event

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -5,7 +5,48 @@ $(document).ready(function () {
   $(".title").text("Hello! Password Generator"); // Generate password when generate button is clicked
 
   $("#generateBtn").click(function (e) {
-    e.preventDefault();
-    $("#passwordDisplay").val(value);
+    e.preventDefault(); // Global Declarations
+
+    var randomPassword = "";
+    var passwordLength = $("#passwordLength").val(); // get password length
+
+    var i; // Generate random characters
+
+    for (i = 0; i < passwordLength; i++) {
+      randomPassword += Math.floor(Math.random() * 9 + 1);
+    }
+
+    $("#passwordDisplay").val(randomPassword);
+
+    if ($("#passwordDisplay").val() != "") {
+      $('#copyPassword').removeAttr('disabled');
+    }
   });
+  $("#passwordLength").keyup(function (e) {
+    e.preventDefault(); // let passwordLength = $("#passwordLength").val();
+
+    if ($(this).val() != '') {
+      $('#generateBtn').removeAttr('disabled');
+    }
+  });
+  $("#copyPassword").click(function (e) {
+    e.preventDefault();
+    var passwordValue = $("#passwordDisplay").val(); // copyPassword()
+
+    alert("Hi copy " + passwordValue);
+    $(this).text("Copied!");
+  }); // let copyPassword = function () {
+  // }
+  // function copyToClipboard(text) {
+  //     var sampleTextarea = document.createElement("textarea");
+  //     document.body.appendChild(sampleTextarea);
+  //     sampleTextarea.value = text; //save main text in it
+  //     sampleTextarea.select(); //select textarea contenrs
+  //     document.execCommand("copy");
+  //     document.body.removeChild(sampleTextarea);
+  // }
+  // function myFunction() {
+  //     var copyText = document.getElementById("myInput");
+  //     copyToClipboard(copyText.value);
+  // }
 });
--- script.js
+++ script.js
@@ -1,15 +1,71 @@
 // PASSWORD GENERATOR
 
 $(document).ready(function () {
-    
+
     $(".title").text("Hello! Password Generator");
 
 
     // Generate password when generate button is clicked
-    $("#generateBtn").click(function (e) { 
+    $("#generateBtn").click(function (e) {
         e.preventDefault();
-        $("#passwordDisplay").val(value);
+
+        // Global Declarations
+        let randomPassword = ""
+        let passwordLength = $("#passwordLength").val(); // get password length
+        let i
+
+        // Generate random characters
+        for (i = 0; i < passwordLength; i++) {
+            randomPassword += Math.floor((Math.random() * 9) + 1);
+        }
 
 
+
+
+
+
+
+        $("#passwordDisplay").val(randomPassword);
+        if ($("#passwordDisplay").val() != "") {
+            $('#copyPassword').removeAttr('disabled');
+        }
     });
+
+
+    $("#passwordLength").keyup(function (e) {
+        e.preventDefault()
+        // let passwordLength = $("#passwordLength").val();
+        if ($(this).val() != '') {
+            $('#generateBtn').removeAttr('disabled');
+        }
+    });
+
+    $("#copyPassword").click(function (e) {
+        e.preventDefault();
+
+        let passwordValue = $("#passwordDisplay").val();
+
+
+        // copyPassword()
+        alert("Hi copy " + passwordValue)
+        $(this).text("Copied!");
+    });
+
+    // let copyPassword = function () {
+
+    // }
+
+    // function copyToClipboard(text) {
+    //     var sampleTextarea = document.createElement("textarea");
+    //     document.body.appendChild(sampleTextarea);
+    //     sampleTextarea.value = text; //save main text in it
+    //     sampleTextarea.select(); //select textarea contenrs
+    //     document.execCommand("copy");
+    //     document.body.removeChild(sampleTextarea);
+    // }
+
+    // function myFunction() {
+    //     var copyText = document.getElementById("myInput");
+    //     copyToClipboard(copyText.value);
+    // }
 });
